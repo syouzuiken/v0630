4e778daac557b116d82e6d3e853f7f8ec932195b
syouzuiken <laybaekzhang@gmail.com>
2022-06-30 13:32:28 +0900

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,39 @@ namespace v0630
             InitializeComponent();
         }
 
+        int vx = -5;
+        int vy=-10;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+
+            if (label1.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+
+            Point spos = MousePosition;
+            Point fpos = PointToClient(spos);
+            label2.Left = fpos.X - label2.Width / 2;
+            label2.Top = fpos.Y - label2.Height / 2; ;
+            
+        }
     }
 }
